36c7078817c1ba6bbe95296252027557e66bf6eb
scottman8265 <scriptjet@gmail.com>
2023-08-02 03:06:46 -0500

    took out the lastInsertID from Process

--- class/Process.php
+++ class/Process.php
@@ -80,9 +80,6 @@ class Process
             $results = $this->error;
         }
 
-        if ($insert || $update) {
-                        $this->lastID = $this->lnk->lastInsertId();
-        }
 
         unset($lnk);
 
