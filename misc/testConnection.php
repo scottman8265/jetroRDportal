e58bdfa2b05ca5408f8672c77a7bcacfcf6df27e
scottman8265 <scriptjet@gmail.com>
2023-08-02 13:57:10 -0500

    oh gosh

--- misc/testConnection.php
+++ misc/testConnection.php
@@ -6,7 +6,7 @@ require_once('../class/Process.php');
 $process = new Process();
 
 // Query the database
-$result = $process->query('SELECT * FROM branchinfo');
+$result = $process->query('SELECT * FROM branches');
 
 // Dump the results
 var_dump($result);
