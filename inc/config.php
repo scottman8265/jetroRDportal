c439fcd0dd09246918d257a388143f68f91e7198
scottman8265 <scriptjet@gmail.com>
2023-08-01 20:52:40 -0500

    added new database config file

--- inc/config.php
+++ inc/config.php
@@ -1,15 +1,15 @@
 <?php
 
     /*Set Server Connect*/
-    define('DB_SERVER', 'localhost:3306');
-    define('DB_USER', 'scriptjet');
+    define('DB_SERVER', 'scriptjetprime.works');
+    define('DB_USER', 'root');
     define('DB_PASS', 'scottM@n8265');
-    define('DB_NAME', 'jrd_stuff');
-    define('DB_2', 'auditAnalysis');
+    define('DB_NAME', 'spwr00!M@n');
+    define('DB_2', 'jrdStuff');
 
     /*Email Settings*/
-    define('EMAIL_TO', 'staylor@jetrord.com');
-    define('EMAIL_FROM_ADDR', 'jetroRDtrainer.com');
+    define('EMAIL_TO', 'scriptjet@scri.ptjet.com');
+    define('EMAIL_FROM_ADDR', 'scriptjetPrime.works');
     define('MESSAGE_SUBJECT', 'A message from scott');
     define('SEND_EMAIL', false); /* if set to true, an email is sent to admin after a message is inserted */
 
