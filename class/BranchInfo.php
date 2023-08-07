4a55a7f0de01dc6547fd396f0379bdc63bc1343f
scottman8265 <scriptjet@gmail.com>
2023-08-07 02:27:51 -0500

    lots of changes... broke BranchInfo.php class from arrays -- wrote JRDaudits.php class from the failed portal i'll try to bring back

---
+++ class/BranchInfo.php
@@ -0,0 +1,415 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Robert Brandt
+ * Date: 1/7/2019
+ * Time: 5:25 AM
+ */
+
+//TODO replace all hardcoded database names with constants from config.php
+//TODO replace all hardcoded table names with constants from config.php
+//TODO add params to all sql statements that can take them
+require_once '../class/Process.php';
+require_once '../inc/config.php';
+
+class BranchInfo extends Process
+{
+
+    /**
+     * Summary of ABS
+     * @var array active branches
+     */
+    private $ABS = [];
+    /**
+     * Summary of LR
+     * @var array location request
+     */
+    private $LR = [];
+    /**
+     * Summary of XD
+     * @var string xDock
+     */
+    private $XD;
+    /**
+     * Summary of SF
+     * @var bool seafood
+     */
+    private $SF;
+    /**
+     * Summary of BN
+     * @var string branchName
+     */
+    private $BN;
+    /**
+     * Summary of RGN
+     * @var string region
+     */
+    private $RGN;
+    /**
+     * Summary of REG
+     * @var string regional
+     */
+    private $REG;
+    /**
+     * Summary of DIR
+     * @var string director
+     */
+    private $DIR;
+    /**
+     * Summary of LT
+     * @var string location type
+     */
+    private $LT;
+    /**
+     * Summary of SMW
+     * @var bool smwares
+     */
+    private $SMW;
+    /**
+     * Summary of WAS
+     * @var bool wine and spirits
+     */
+    private $WAS;
+
+    /**
+     * Summary of RIS
+     * @var array
+     */
+    private $RIS = [];
+    /**
+     * Summary of BNS
+     * @var array branch numbers
+     */
+    private $BNS = [];
+    /**
+     * Summary of ARS
+     * @var array active request items
+     */
+    private $ARS = [];
+
+    private $DBLU = ["BI" => ["db" => "jrdStuff.BRINF", "w" => 'BID'], "SF" => ["db" => 'jrdStuff.BRINF', "w" => "SF"], "XD" => ["db" => 'jrdStuff.XDS', "w" => "XDID"], "BN" => ["db" => 'jrdStuff.BNS', "w" => "BNID"], "RGN" => ["db" => 'jrdStuff.RGNS', "w" => "RGNID"], "REG" => ["db" => 'jrdStuff.FTMS', "w" => "TMID"], "DIR" => ["db" => 'jrdStuff.FTMS', "w" => "TMID"], "LT" => ["db" => 'jrdStuff.LTS', "w" => "LTID"], "SMW" => ["db" => 'jrdStuff.BRINF', "w" => "SMW"], "WAS" => ["db" => 'jrdStuff.BRINF', "w" => "WAS"]];
+
+    /**
+     * Summary of __construct
+     * @param array $BNS branch numbers
+     * @param array $RIS request items
+     */
+    public function __construct($BNS = null, $RIS = null)
+    {
+        parent::__construct();
+
+        $this->setupLR([$BNS, $RIS]);
+    }
+
+    /**
+     * Summary of getBi
+     * @param int $bn branch number
+     * @param string $ri field to query
+     * @return mixed $r result
+     */
+    private function getBi($bn, $ri)
+    {
+        $r = null;
+
+        $s = "SELECT ? FROM jrdStuff.branchInfo WHERE branchId = ?";
+        $p = [$ri, $bn];
+        $r = Process::query($s, $p);
+
+        return $r;
+    }
+
+    private function queryRI($ri)
+    {
+        $r = null;
+
+        $s = "SELECT ? FROM ? WHERE ? = ?";
+        $p = [$ri[0], $ri[1], $ri[2], $ri[3]];
+        $r = Process::query($s, $p);
+
+        return $r;
+    }
+
+
+
+    /**
+     * Summary of requestBranchInfo
+     * @param array $x array of [branch numbers, request items]
+     * @return void
+     */
+    public function requestBranchInfo($x)
+    {
+        $this->setupLR($x);
+
+        return $this->answerLR();
+    }
+    private function setupLR($z)
+    {
+        $this->setBNS($z[0]);
+        $this->setRIS($z[1]);
+        $this->setLR();
+    }
+
+    private function setDBLU($ri)
+    {
+        $d = [];
+
+        $d = $this->DBLU[$ri];
+
+        $this->DBLU = $d;
+    }
+
+    private function answerLR()
+    {
+        $d = [];
+        foreach ($this->getLR() as $bn => $ris) {
+            foreach ($ris as $ri => $riid) {
+                $d[$bn][$ri] = $this->queryRI([$ri, $this->DBLU[$ri][0], $this->DBLU[$ri][1], $riid]);
+            }
+        }
+        $this->LR = $d;
+    }
+
+    /**
+     * Summary of setLR
+     * @param array $dblu [database.table, w clause]
+     * @var int $o branch number
+     * @var string $g request item
+     * @var array $d array[branch number][request item][request item lookup value]
+     * @return void
+     */
+    private function setLR()
+    {
+        $d = [];
+
+        foreach ($this->getBNS() as $o) {
+            foreach ($this->getRIS() as $g) {
+                $dblu = $this->DBLU[$g];
+                $d[$o][$g] = $this->queryRI([$g, $dblu[0], $dblu[1], $o]);
+            }
+        }
+        $this->LR = $d;
+    }
+
+    private function getLR()
+    {
+        if (empty($this->LR)) {
+            $this->setLR();
+        }
+
+        return $this->LR;
+    }
+
+    /**
+     * Summary of setBNS
+     * @param array $BNS branch numbers
+     * @return void
+     */
+    private function setBNS($BNS = null)
+    {
+        $this->BNS = $BNS == null ? $this->getABS() : $BNS;
+    }
+
+    /**
+     * Summary of getBNS
+     * @return array $BNS branch numbers
+     */
+    public function getBNS()
+    {
+        if (empty($this->BNS)) {
+            $this->setBNS();
+        }
+        return $this->BNS;
+    }
+
+    /**
+     * Summary of setABS
+     * @var array $a active branches
+     * @var string $s s
+     * @var Process::query $q query
+     * @var array $r result
+     * @return void ABS active branches
+     */
+    private function setABS()
+    {
+        $a = [];
+
+        $s = "SELECT branchId AS branchNum FROM jrdStuff.branchInfo WHERE active = 1 SORT BY branchNum ASC";
+
+        $q = parent::query($s);
+
+        foreach ($q as $r) {
+            $a[] = $r['branchNum'];
+        }
+
+        $this->ABS = $a;
+    }
+
+    /**
+     * Summary of getABS
+     * @return array $ABS active branches
+     */
+    public function getABS()
+    {
+        if (empty($this->ABS)) {
+            $this->setABS();
+        }
+        return $this->ABS;
+    }
+
+    /**
+     * Summary of setRIS
+     * @param mixed $RIS request items
+     * @return void
+     */
+    private function setRIS($RIS = null)
+    {
+        $this->RIS = $RIS == null ? $this->getARS() : $RIS;
+    }
+
+    public function getRIS()
+    {
+        if (empty($this->RIS)) {
+            $this->setRIS();
+        }
+        return $this->RIS;
+    }
+
+    public function setARS()
+    {
+
+        //TODO take out the hard coded a and replace with dynamic a from database
+        $this->ARS = ['BN', 'RGN', 'REG', 'DIR', 'LT', 'SF', 'SMW', 'WAS', 'XD'];
+    }
+
+    private function getARS()
+    {
+        if (empty($this->ARS)) {
+            $this->setARS();
+        }
+        return $this->ARS;
+    }
+
+    private function setBN($x)
+    {
+        $s = "SELECT branchName FROM jrdStuff.branchNames WHERE branchNameId = " . $this->getBi($x, 'branchName');
+        $this->BN = Process::query($s);
+    }
+
+    public function getBN($x)
+    {
+        if (!$this->BN) {
+            $this->setBN($x);
+        }
+        return $this->BN;
+    }
+
+    private function setRGN($x)
+    {
+        $s = "Select region FROM jrdStuff.regions WHERE regionId = " . $this->getBi($x, 'region');
+        $this->RGN = Process::query($s);
+    }
+
+    public function getRGN($x)
+    {
+        if (!$this->RGN) {
+            $this->setRGN($x);
+        }
+        return $this->RGN;
+    }
+
+    private function setREG($x)
+    {
+        $s = "Select ftmName FROM jrdStuff.fieldTeamMembers WHERE tmId = " . $this->getBi($x, 'regional');
+        $this->REG = Process::query($s);
+    }
+
+    public function getREG($x)
+    {
+        if (!$this->REG) {
+            $this->setREG($x);
+        }
+        return $this->REG;
+    }
+
+    private function setDIR($x)
+    {
+        $s = "Select DIR FROM jrdStuff.fieldTeamMembers WHERE tmId = " . $this->getBi($x, 'DIR');
+        $this->DIR = Process::query($s);
+    }
+
+    public function getDIR($x)
+    {
+        if (!$this->DIR) {
+            $this->setDIR($x);
+        }
+        return $this->DIR;
+    }
+
+    private function setXD($x)
+    {
+        $s = "Select XD FROM jrdStuff.xDocks WHERE xDockId = " . $this->getBi($x, 'XD');
+        $this->XD = Process::query("Select XD FROM jrdStuff.xDocks WHERE xDockId = " . $x);
+    }
+
+    public function getXD($x)
+    {
+        if (!$this->XD) {
+            $this->setXD($x);
+        }
+        return $this->XD;
+    }
+
+    private function setSF($x)
+    {
+        $this->SF = $this->getBi($x, 'SF');
+    }
+
+    public function getSF($x)
+    {
+        if (!$this->SF) {
+            $this->setSF($x);
+        }
+        return $this->SF;
+    }
+
+    private function setSMW($x)
+    {
+        $this->SMW = $this->getBi($x, 'SMW');
+    }
+
+    public function getSMW($x)
+    {
+        if (!$this->SMW) {
+            $this->setSMW($x);
+        }
+        return $this->SMW;
+    }
+
+    private function setLT($x)
+    {
+        $s = "Select LT FROM jrdStuff.locationTypes WHERE locationTypeId = " . $this->getBi($x, 'LT');
+        $this->LT = Process::query($s);
+    }
+
+    public function getLT($x)
+    {
+        if (!$this->LT) {
+            $this->setLT($x);
+        }
+        return $this->LT;
+    }
+
+    private function setWAS($x)
+    {
+        $this->WAS = $this->getBi($x, 'WAS');
+    }
+
+    public function getWAS($x)
+    {
+        if (!$this->WAS) {
+            $this->setWAS($x);
+        }
+        return $this->WAS;
+    }
+}
---
+++ class/JRDaudits.php
@@ -0,0 +1,401 @@
+<?php
+
+//TODO write scores, findings & people to database
+
+require_once('..\inc\config.php');
+require_once('..\class\Process.php');
+
+/**
+ * Class JRDaudits
+ * Handles audit processing with various operations related to corporate and self audits.
+ */
+class JRDaudits extends Process
+{
+
+    private $sheet;
+    private $totalScoreCell = array();
+    /**
+     * Summary of version
+     * @var
+     */
+    public $version = null;
+    /**
+     * Summary of positions
+     * @var
+     */
+    private $positions = array();
+    /**
+     * Summary of findings
+     * @var
+     */
+    public $findings = array();
+    /**
+     * Summary of auditLookup
+
+     * @var
+     */
+    public $auditLookup = array();
+
+    /**
+     * Summary of findingStart
+     * @var int
+     */
+    public $findingStart = 0;
+
+    /**
+     * Summary of figuresStartRow
+     * @var int
+     */
+    private $figuresStartRow = 0;
+
+    /**
+     * Summary of people
+     * @var array
+     */
+    public $people = array();
+
+    /**
+     * Summary of scores
+     * @var array
+     */
+    public $scores = array();
+    public function __construct($function = null)
+    {
+        parent::__construct();
+
+        if ($function != null) {
+            $function['name'] == 'newCorpAudit' ? $this->processNewCorpAudit($function['sheet']) : null;
+        }
+    }
+
+
+    /**
+     * Summary of processNewCorpAudit
+     * @param PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $this->sheet
+     * @return void
+     */
+    public function processNewCorpAudit(\PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $sheet)
+    {
+        $initializeErrors = [];
+
+        $this->sheet = $sheet;
+        $this->sheet == null ? $initializeErrors[] = "There was an error in getting your spreadsheet. Make sure the file is in the correct format and name correctly ([YEAR] [QUARTER] 'CORP' [BRANCH NUMBER] [BRANCH NAME])" : null;
+
+        $this->initializeTotalScoreCell();
+        $this->getFiguresStartRow() == null ? $initializeErrors[] = "There was an error in reading the information from your spreadsheet. Make sure you are using a recent audit and it has not been altered from it's original structure for the used version.)" : null;
+
+        $this->initializeVersion();
+        $this->getVersion() == null ? $initializeErrors[] = "There was an error in eading the version from your spreadsheet. Make sure you are using a recent audit and it has not been altered from it's original structure for the used version.)" : null;
+
+        $this->initializeAuditLookup();
+        $this->getAuditLookup() == null ? $initializeErrors[] = "There was an error in getting the audits from the database. This is an internal error and if it continues please utilize this sites help function for assistance." : null;
+
+        $this->initializePositions();
+        $this->getPeople() == null ? $initializeErrors[] = "There was an error in getting the people from the database. Please verify that you do not have any unauthorized characters in any of the fields listing the department heads at the top of the Audit Recap (only A-Z, 0-9, '-', '_', '.' is allowed)." : null;
+
+        $this->initializeFindings();
+        $this->initializeScores();
+    }
+
+    /**
+     * Initializes totalScoreCell by finding "Total Score" and breaking down the cell into parts.
+     */
+    private function initializeTotalScoreCell()
+    {
+        // Search in column H for "Total Score"
+        $highestRow = $this->sheet->getHighestRow();
+        for ($row = 1; $row <= $highestRow; $row++) {
+            if ($this->sheet->getCellByColumnAndRow(8, $row)->getValue() === "Total Score") {
+                $this->totalScoreCell = [
+                    'row' => $row,
+                    'col' => 8,
+                ];
+                break;
+            }
+        }
+        $this->setFiguresStartRow();
+    }
+
+    /**
+     * Initializes figuresStartRow based on the totalScoreCell.
+     */
+    private function setFiguresStartRow()
+    {
+        $this->figuresStartRow = $this->totalScoreCell['row'] + 1; // Modify as needed
+    }
+
+    /**
+     * Gets the figuresStartRow.
+     * @return int
+     */
+    private function getFiguresStartRow()
+    {
+        return $this->figuresStartRow;
+    }
+
+    /**
+     * Sets positions with an optional receivedPositions array.
+     * @return void
+     */
+    private function initializePositions()
+    {
+        $this->positions = array(
+            'Branch Manager' => [4, 14],
+            'auditor' => [3, 14],
+            'Sr ABM' => [5, 14],
+            'Jr ABM' => [6, 14],
+            'Hradmin' => [0, 27],
+            'Cashroom' => [1, 27],
+            'Deli Mgr' => [2, 27],
+            'Floor Mgr' => [3, 27],
+            'Front End Mgr' => [4, 27],
+            'Gen Ops (NABM)' => [5, 27],
+            'Inv Control' => [6, 27],
+            'Meat Mgr' => [0, 38],
+            'Pest (NABM)' => [1, 38],
+            'Produce Mgr' => [2, 38],
+            'Receiving Mgr' => [3, 38],
+            'Receptionist (TOA)' => [4, 38],
+            'Safety Mgr (NABM)' => [5, 38],
+            'Seafood Mgr' => [6, 38],
+            'Smallwares Mgr' => [0, 51],
+            'Wine Steward' => [1, 51],
+            'Branch Manager #2' => [2, 51],
+            'ABM #3' => [3, 51]
+        );
+        $this->setPeople();
+    }
+
+    /**
+     * Summary of getPositions
+     * @return array
+     */
+    public function getPositions()
+    {
+        return $this->positions;
+    }
+
+    /**
+     * Summary of updatePositions
+     * @param array $updatedPositions
+     * @return void
+     */
+    public function updatePositions($updatedPositions)
+    {
+        foreach ($updatedPositions as $positionName => $offset) {
+            $this->positions[$positionName] = $offset;
+        }
+    }
+
+    /**
+     * Summary of setPeople
+     * @return void
+     */
+    private function setPeople()
+    {
+        $people = array();
+        foreach ($this->getPositions() as $positionName => $offset) {
+            $row = $this->totalScoreCell['row'] + $offset['row'];
+            $col = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($this->totalScoreCell['column']) + $offset['col'];
+
+            $tmInPosition = $this->sheet->getCellByColumnAndRow($col, $row)->getValue();
+
+            $people[$positionName] = $tmInPosition;
+        }
+        $this->people = $people;
+    }
+
+    /**
+     * Summary of getPeople
+     * @return array
+     */
+    public function getPeople()
+    {
+        return $this->people;
+    }
+
+    /**
+     * Sets the version by offsetting the totalScoreCell.
+     */
+    public function initializeVersion()
+    {
+        // Assuming $this->totalScoreCell has been initialized as an array with 'row' and 'col'
+        $row = $this->totalScoreCell['row'] + 4;
+        $col = $this->totalScoreCell['col'] + 50;
+
+        // Retrieve the value from the sheet
+        $this->version = $this->sheet->getCellByColumnAndRow($col, $row)->getValue();
+    }
+
+    /**
+     * Summary of getVersion
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Summary of setQuesArray
+     * @return void
+     */
+    public function initializeAuditLookup()
+    {
+        if ($this->version == null) {
+            $this->initializeVersion();
+        }
+
+        $array = [];
+
+        $sql = "SELECT auditCode, auditName FROM jrd_stuff.auditlookup ORDER BY auditCode ASC";
+        $qry = Process::query($sql);
+
+        foreach ($qry as $info) {
+            $array[$info['auditName']] = $info['auditCode'];
+        }
+
+        $this->auditLookup = $array;
+    }
+
+    /**
+     * Summary of getQuesArray
+     * @return array
+     */
+    public function getAuditLookup()
+    {
+        return $this->auditLookup;
+    }
+
+    /**
+     * Sets findings with an optional getRepeats boolean.
+     * @param bool $getRepeats
+     */
+    function initializeFindings($getRepeats = true)
+    {
+        $audits = $this->getAuditLookup();
+        $findings = array();
+
+        $rowMax = $this->totalScoreCell['row'] - 2;
+        $findingStart = $this->totalScoreCell['row'] + 27;
+
+        // Processing findings code...
+
+        if ($rowMax != null) {
+            for ($i = 2; $i < $rowMax; $i++) {
+                $qNum = $this->sheet->getCellByColumnAndRow(6, $i)->getValue();
+                if ($qNum != null) {
+                    $qAudit = $this->sheet->getCellByColumnAndRow(9, $i)->getValue();
+                    $qComm = $this->sheet->getCellByColumnAndRow(11, $i)->getOldCalculatedValue();
+                    $response = $this->sheet->getCellByColumnAndRow(3, $i)->getOldCalculatedValue();
+                    $code = $audits[$qAudit] . (string) $qNum . "." . $this->version;
+
+                    if ($response) {
+                        $findings[$code]['comm'] = $qComm;
+                    } elseif ($audits[$qAudit] == "FL") {
+                        $varArray = [1, 2, 3, 4, 5, 8];
+                        if (in_array($qNum, $varArray) == true && strlen($qComm) > 1) {
+                            $findings[$code]['comm'] = trim($qComm);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if ($getRepeats) {
+            $this->findRepeats($findings);
+        }
+
+        $this->findings = $findings; // Assigning to the class property
+    }
+
+
+    /**
+     * Summary of findRepeats
+     * @param mixed $findings
+     * @return void
+     */
+    private function findRepeats(&$findings)
+    {
+        $count    = 0;
+        $testCode = 'FFFFFFFF';
+
+        foreach ($findings as $code => $value) {
+            $commentCell = "H" . ($this->findingStart + ($count * 8) + 2);
+            $hashCode = $this->sheet->getCell($commentCell)->getStyle()->getFill()->getStartColor()->getARGB();
+
+            if ($hashCode != $testCode) {
+                $findings[$code]['rep'] = 1;
+            } else {
+                $findings[$code]['rep'] = 0;
+            }
+
+            $count++;
+        }
+    }
+
+
+    /**
+     * Sets scores based on the provided details.
+     */
+    public function initializeScores()
+    {
+        $baseStartRow = $this->getFiguresStartRow();
+        $array = array();
+
+        // Define cell locations
+        $r_totScoreLoc = "L" . $baseStartRow;
+        $b_totScoreLoc = "H" . $baseStartRow;
+        $r_freshScoreLoc = "L" . ($baseStartRow + 4);
+        $b_freshScoreLoc = "H" . ($baseStartRow + 4);
+        $deptScoreStart = $baseStartRow + 8;
+        $deptScoreEnd = $baseStartRow + 14;
+        $foodSafety = $baseStartRow + 15;
+        $totArray = ['Z' . ($baseStartRow + 15), 'AH' . ($baseStartRow + 15), 'AP' . ($baseStartRow + 15), 'AX' . ($baseStartRow + 15)];
+        $repCol = "N";
+        $baseCol = "L";
+
+        // Extract and process scores
+        try {
+            $r_array[] = $this->sheet->getCell($r_totScoreLoc)->getOldCalculatedValue();
+            $b_array[] = $this->sheet->getCell($b_totScoreLoc)->getOldCalculatedValue();
+            $r_array[] = $this->sheet->getCell($r_freshScoreLoc)->getOldCalculatedValue();
+            $b_array[] = $this->sheet->getCell($b_freshScoreLoc)->getOldCalculatedValue();
+
+            for ($i = $deptScoreStart; $i < $deptScoreEnd; $i++) {
+                $r_cell = $repCol . $i;
+                $b_cell = $baseCol . $i;
+                $b_score = $this->sheet->getCell($b_cell)->getOldCalculatedValue();
+                $r_score = $this->sheet->getCell($r_cell)->getOldCalculatedValue();
+                $baseScore = $b_score === 'N/A' ? -1 : $b_score;
+                $repScore = $r_score === 'N/A' ? -1 : $r_score;
+                $r_array[] = $repScore;
+                $b_array[] = $baseScore;
+            }
+
+            $r_fsCell = $repCol . $foodSafety;
+            $b_fsCell = $baseCol . $foodSafety;
+            $r_array[] = $this->sheet->getCell($r_fsCell)->getOldCalculatedValue();
+            $b_array[] = $this->sheet->getCell($b_fsCell)->getOldCalculatedValue();
+
+            foreach ($totArray as $tot) {
+                $r_array[] = $this->sheet->getCell($tot)->getOldCalculatedValue();
+                $b_array[] = $this->sheet->getCell($tot)->getOldCalculatedValue();
+            }
+        } catch (\PhpOffice\PhpSpreadsheet\Exception $e) {
+            echo $e->getMessage();
+        }
+
+        $r_array[] = 1;
+        $b_array[] = 0;
+
+        $array = ['rep' => $r_array, 'base' => $b_array];
+
+        $this->scores = $array;
+    }
+
+
+
+    // Additional methods and logic as needed
+}
